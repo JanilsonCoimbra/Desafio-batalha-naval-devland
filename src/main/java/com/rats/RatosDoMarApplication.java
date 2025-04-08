fd9b6321d2535f2b64e15de46cc6389a98326f2e
EngMateusCardoso <mateusoliver490@gmail.com>
2025-04-08 03:46:52 -0300

    atack changes

--- src/main/java/com/rats/RatosDoMarApplication.java
+++ src/main/java/com/rats/RatosDoMarApplication.java
@@ -1,6 +1,4 @@
 package com.rats;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
