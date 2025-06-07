2678392321d0c8784030ef72b4843107315be5de
Jacques <beatriztamie@icloud.com>
2025-06-06 21:35:47 -0300

    alteração e melhorias -no HadleAttackResult

--- src/main/java/com/rats/services/handlers/HandleAttackResult.java
+++ src/main/java/com/rats/services/handlers/HandleAttackResult.java
@@ -1,5 +1,4 @@
 package com.rats.services.handlers;
-import java.io.ObjectInputFilter.Config;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -13,90 +12,55 @@ import com.rats.models.AttackResultContent;
 import com.rats.models.ShipModel;
 import com.rats.validations.CalculadoraDeBatalha;
 public class HandleAttackResult implements IHandleChain {
-    
+
     private IHandleChain nextHandler;
-    ShipModel shipModel = ShipModel.getShipModel();
-    
-    
-    @Override
-    public IHandleChain next(IHandleChain nextHandler) {
-        this.nextHandler = nextHandler;
-        return this.nextHandler;
+    private ShipModel shipModel;
+
+    public HandleAttackResult(ShipModel shipModel) {
+        this.shipModel = shipModel;
     }
+
+        @Override
+        public IHandleChain next(IHandleChain nextHandler) {
+            this.nextHandler = nextHandler;
+            return this.nextHandler;
+        }
     
-    @Override
-    public ICommunication validate(ICommunication payload) {
-        
-        if (payload.getEvento() == EventsEnum.ResultadoAtaqueEfetuado && payload.getNavioDestino().equals(Configs.SUBSCRIPTION_NAME)) {
+        @Override
+        public ICommunication validate(ICommunication request) {
+
+            if (request.getEvento() == EventsEnum.ResultadoAtaqueEfetuado && request.getNavioDestino().equals(Configs.getInstance().SUBSCRIPTION_NAME)) {
+                HandleLog.title("Ataque recebido: ");
+
+                ObjectMapper objectMapper = new ObjectMapper();
+                objectMapper.setSerializationInclusion(com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL);
+                
                 try {
-                    ObjectMapper objectMapper = new ObjectMapper();
-                    objectMapper.setSerializationInclusion(com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL);
                     
-                    AttackResultContent messageReceived = objectMapper.readValue(payload.getConteudo().toString(), AttackResultContent.class);
-
-                    if(messageReceived.getDistanciaAproximada() == 5 && shipModel.getShootLevel() == 0){
-                        messageReceived.getPosicao().getX();
-                        messageReceived.getPosicao().getY();
-                        Configs.FIRST_SET_SHOOT_FIVE = new ArrayList<>();
-                        Configs.FIRST_SET_SHOOT_FIVE.add(Math.toIntExact(messageReceived.getPosicao().getX() - 1));
-                        Configs.FIRST_SET_SHOOT_FIVE.add(Math.toIntExact(messageReceived.getPosicao().getY()));
-
-                        if (nextHandler != null) {
-                            return nextHandler.validate(payload);
-                        }
-                    }
-                    if (messageReceived.getDistanciaAproximada() >= 1 && messageReceived.getDistanciaAproximada() != 5  && messageReceived.getDistanciaAproximada() <= 7 && shipModel.getShootLevel() == 0) {
-                        HandleLog.title("SET SHOOT LEVEL 1: ");
+                    AttackResultContent messageReceived = objectMapper.readValue(request.getConteudo().toString(), AttackResultContent.class);
+                    if (messageReceived.getDistanciaAproximada() <= 7) {
                         shipModel.setShootLevel(1);
+
+                        System.out.println("Distancia menor que 7: " + messageReceived.getDistanciaAproximada());
+
                         shipModel.distanceApproximate = String.valueOf(messageReceived.getDistanciaAproximada());
-
-                        List<long[]> wrappedPositions = CalculadoraDeBatalha.calcularPosicoesPossiveis(messageReceived.getPosicao().getX(), messageReceived.getPosicao().getY(), messageReceived.getDistanciaAproximada());
-                        Configs.POSITION_X_RED_SHOOT = Math.toIntExact(messageReceived.getPosicao().getX());
-                        Configs.POSITION_Y_RED_SHOOT = Math.toIntExact(messageReceived.getPosicao().getY());
-                    
-                        Configs.SECOND_SET_SHOOT = new ArrayList<>();
-                        wrappedPositions.forEach(item -> {
-                            System.out.println("Posicoes possiveis level 1: "+Arrays.toString(item));
-                            Configs.SECOND_SET_SHOOT.add(Arrays.asList((int)item[0], (int)item[1]));
-                        });
-
-                        if (nextHandler != null) {
-                            return nextHandler.validate(payload);
-                        }
-                    } 
-                    
-                    if (messageReceived.isAcertou() && (shipModel.getShootLevel() == 1 || shipModel.getShootLevel() == 0)) {
-                        shipModel.setShootLevel(2);
-
-                        payload.getPontuacaoNavios().forEach((key, value) -> {
-                            System.out.println("Naviooo : " + key + " - Pontuação: " + value);
-                            if(!key.equals(Configs.SUBSCRIPTION_NAME)) {
-                                Configs.enemyScore = value;
-                            }
-                        });
-
-                        System.out.println("Acertou a posição e o nível de tiro é 1 ou 0: ");
-                        HandleLog.title("Acertou: " + messageReceived.isAcertou());
+                        List<Long[]> wrappedPositions = new ArrayList<>();
+                        CalculadoraDeBatalha.calcularPosicoesPossiveis(messageReceived.getPosicao().getX(), messageReceived.getPosicao().getY(), messageReceived.getDistanciaAproximada())
+                            .forEach(pos -> wrappedPositions.add(Arrays.stream(pos).boxed().toArray(Long[]::new)));
+                            shipModel.secondSetShoot.add(wrappedPositions);
                         
-                        List<long[]> wrappedPositions = CalculadoraDeBatalha.calculateThirdSetShoot(messageReceived.getPosicao().getX(), messageReceived.getPosicao().getY());
-                        
-                        Configs.THIRD_SET_SHOOT = new ArrayList<>();
-                        wrappedPositions.forEach(item -> {
-                            System.out.println("Posicoes possiveis level 2: "+Arrays.toString(item));
-                            if((int)item[0] >= 1 && (int)item[0] <= 100 && (int)item[1] >= 1 && (int)item[1] <= 30) 
-                                Configs.THIRD_SET_SHOOT.add(Arrays.asList((int)item[0], (int)item[1]));
-                        }); 
                     }
                 } catch (com.fasterxml.jackson.core.JsonProcessingException e) {
-                    throw new RuntimeException("Error processing JSON", e);
+                    System.err.println("Error processing JSON: " + e.getMessage());
                 }
             }
+
             if (nextHandler != null) {
-                return nextHandler.validate(payload);
+                return nextHandler.validate(request);
 
             }
             
-            return payload;
+            return request;
         }
 
 }
